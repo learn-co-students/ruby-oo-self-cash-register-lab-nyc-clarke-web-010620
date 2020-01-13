40b301e3856e059d3b0c8e5abd55e48cee796fb3
arichards4814 <arichards4814@gmail.com>
2020-01-12 20:45:50 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,55 @@
 
+require 'pry'
+class CashRegister
+
+    attr_accessor :total
+
+    def initialize (discount = 0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def discount
+        @discount
+    end
+
+    def add_item(item, price, quantity = 1)
+        new_item = {}
+        
+        new_item["item"] = item
+        new_item["price"] = price
+        new_item["quantity"] = quantity
+        
+        @items << new_item
+
+        @total = (new_item["price"] * new_item["quantity"]) + @total
+
+        @total
+    end
+
+    def apply_discount
+        if @discount > 0 
+        discount = @total * @discount * 0.01
+        @total = @total - discount
+        "After the discount, the total comes to $#{@total.to_i}."
+        else 
+        "There is no discount to apply."
+        end
+    end
+
+    def items
+        all_items = []
+
+        @items.each do |item|
+            
+            
+        end
+
+
+    end
+
+
+
+
+end
